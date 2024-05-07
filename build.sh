60458c5e3a40db2749ee8fdd8045bfc791c0b3fa
Matti Paksula <matti.paksula@iki.fi>
2024-05-07 23:01:05 +0300

    b

--- build.sh
+++ build.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/env bash
 
+set -eEuo pipefail
+
+_log() {
+  echo "KUUTEE: $*"
+}
+
+export BUILDKIT_PROGRESS=plain
+
 services="ubuntu2004 ubuntu2404 ubuntu2204 ubuntu1804"
 
 for service in $services
 do
-  docker compose build "$service" || echo "failed"
-  echo ""
+  _log "building $service"
+  if docker compose build "$service"
+  then
+    _log "$service build ok"
+  else
+    _log "$service build failed"
+  fi
 done
+
+_log "done"
